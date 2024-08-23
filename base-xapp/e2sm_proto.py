3ebfbb39e19fb67d71ed18f514a0beb42d0e947a
Alberto Ceresoli <100763253+AlbertoCeresoli@users.noreply.github.com>
2024-08-23 16:11:43 +0200

    Update e2sm_proto.py

--- base-xapp/e2sm_proto.py
+++ base-xapp/e2sm_proto.py
@@ -1,7 +1,20 @@
 from sys import path
 from os import getcwd
 
-cwd = getcwd()
-path.append('{}/base-xapp/oai-oran-protolib/builds'.format(cwd))
+
+cwd = getcwd() #restituisce la directory corrente 
+"""
+sys.path è una lista di directories dove l'interprete python cerca moduli
+"""
+# Si aggiunge il percorso alla cartella Protolib1/builds
+
+path.append('{}/base-xapp/oai-oran-protolib/builds'.format(cwd)) 
+
+
+# Infatti il file ran_messages pb2.py si trova in quella cartella
 tmp = __import__('ran_messages_pb2')
+# L'import effettuato in questo modo permette di rinominare la libreria
+
+
 globals().update(vars(tmp))
+#Aggiorna le variabili globali con le variabili della libreria ran_messages_pb2
